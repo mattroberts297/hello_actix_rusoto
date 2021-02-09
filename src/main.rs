34ec96510b389b4d2cc958dfecb01edc3752c213
Matt Roberts <mattroberts297@users.noreply.github.com>
2021-02-09 20:11:11 +0000

    Cut down main for stack overflow

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,21 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
-
 use std::default::Default;
-
 use rusoto_core::Region;
 use rusoto_dynamodb::{DynamoDb, DynamoDbClient, ListTablesInput};
 
 #[get("/tables")]
 async fn tables(_req_body: String) -> impl Responder {
-    // DynamoDB.
-    let region = match std::env::var("AWS_REGION") {
-        Ok(r) => if String::eq(&r, &String::from("local")) {
-            Region::Custom { name: String::from("eu-west-2"), endpoint: String::from("http://localhost:8000") }
-        } else {
-            Region::default()
-        }
-        Err(_e) => Region::default()
-    };
-
-    let client = DynamoDbClient::new(region);
+    let client = DynamoDbClient::new(Region::default());
     let list_tables_input: ListTablesInput = Default::default();
-
     match client.list_tables(list_tables_input).await {
-        Ok(output) => {
-            match output.table_names {
-                Some(table_name_list) => {
-                    match table_name_list.get(0) {
-                        Some(table_name) => HttpResponse::Ok().body(table_name),
-                        None => HttpResponse::NotFound().finish()
-                    }
-                },
-                None => {
-                    println!("No tables in database!");
-                    HttpResponse::NotFound().finish()
-                },
-            }
-        },
-        Err(error) => {
-            println!("Error: {:?}", error);
-            HttpResponse::InternalServerError().finish()
-        }
+        Ok(_output) => HttpResponse::Ok().finish(),
+        Err(_error) => HttpResponse::InternalServerError().finish(),
     }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(tables)
-    })
+    HttpServer::new(|| { App::new().service(tables) })
     .bind("0.0.0.0:8080")?
     .run()
     .await
